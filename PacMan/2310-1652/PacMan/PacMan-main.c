0a7bb52b2012860de41a707213f607959344c863
Mario-jpeg <m.laurato04@gmail.com>
2024-10-23 16:56:14 +0200

    risoluzione bug minori
    ho aggiunto il controllo se pac man va verso un fantasma, appena posso metto il controllo contrario. Aggiungerò anche una flag che mostra lo stato del power up e vedo di sistemare finalmente quei movimenti dei fantasmi con anche l'azzeramento del random

--- PacMan/2310-1652/PacMan/PacMan-main.c
+++ PacMan/2310-1652/PacMan/PacMan-main.c
@@ -4,9 +4,158 @@
 #include <conio.h>
 #include <ctype.h>
 #include <stdbool.h>
+/*
+	this are the global variable
+*/
+	int click;
+	
+/*
+		THESE ARE THE FUNCTION THAT HAVE THE CONTROL OF THE MAP
+*/
 
+// define the dimension of the game pitch	
+	#define ROWS 19 // Y coordinates
+	#define COLS 17 // X coordinates
+
+
+// configure the map at the initial condition
+	void initMap(); 
+	
+// check if the map is correctly constructed
+	void integrityCheck();
+
+// set the border of the map
+	void setBoundry();
+
+// set the walls inside the map
+	void setWalls(); 
+	
+// set the initial position of PacMan
+	void setPacMan();
+
+// set the initial position of the ghosts
+	void setGhost();
+
+// set dots inside the map
+	void setDot();
+//
+	void setEnergizer();
+	
+// set the fruit in the map
+	void setFruit();
+
+//	
+	int cp_xFruit = 8;
+	int cp_yFruit = 11;	
+	
+//
+	void gameOver();
+	
+// show the current state of the map
+	void displayMap();
+
+// array containing the map itself
+	char map[ROWS][COLS];
+
+/*
+		THESE ARE THE FUNCTION THAT HAVE THE CONTROL OF PACMAN
+*/
+
+/* setting the current position (cp) of PacMan
+   in particular these are the starting position */
+	int cp_x = 8;
+	int cp_y = 14;
+
+/* this counter is used for the activation of the ghost
+   in base of the numper of steps PacMan has done */
+	int counter = 0; 
+	int timeToEat = 0;
+	//POSSIBLE SUBJECT TO TOTAL OVERHAUL
+
+/* */
+	bool eatable = false;
+/* */
+	bool fruitEaten = false;
+
+/* this counter is the score of the player */
+	int score = 0;
+	
+// move is the function that allow the game to be run
+	void move();
+		
+		/* these function are the move set of PacMan
+		   each one of those checks if is possible to 
+		   move in their specified direction and "eats"
+		   the "object" that occupies that space */ 
+		void up();
+		void down();
+		void left();
+		void right();
+	
+/*
+		THESE ARE THE FUNCTION THAT HAVE THE CONTROL OF THE GHOSTS
+*/
+
+//movement of the ghost
+	
+	/*  it needs for the swap of "object that are in the
+	 	way of the ghosts, because the ghosts do not eat
+		"objects" like PacMan" */
+		//char tmp;  
+		int ghostEated = 0;
+		bool redEatable = false;
+		bool pinkEatable = false;
+		bool blueEatable = false;
+		bool orangeEatable = false; 
+	
+	/*  it needs for the "AI" of the ghosts.
+	 	It is a random number picked in a pool that goes from 1 to 6 
+		that "decide" in which direction the ghosts will go */
+		int odds;
+
+	/*	define a character for each ghost*/
+		char RED = 'R', BLUE = 'B', PINK = 'P', ORANGE = 'O';
+
+
+
+	/* 	these function are the move set of the ghosts
+	    each one of those checks if is possible to 
+	    move in their specified direction and swap
+	    the "object" that occupies that space with the ghost itself */
+		void goUp(int** x, int** y, char ghost);
+		void goDown(int** x, int** y, char ghost);
+		void goLeft(int** x, int** y, char ghost);
+		void goRight(int** x, int** y, char ghost);
+
+//red
+	/*  setting the current position (cp) of the "RED" ghost
+   		in particular these are the starting position */
+		int cp_xRed = 8;
+		int cp_yRed = 7;
+
+//pink
+	/*  setting the current position (cp) of "PINK" ghost
+   		in particular these are the starting position */
+		int cp_xPink = 8;
+		int cp_yPink = 9;
+
+//blue
+	/*  setting the current position (cp) of "BLUE" ghost
+   		in particular these are the starting position */
+		int cp_xBlue = 7;
+		int cp_yBlue = 9;
+		
+//orange
+	/*  setting the current position (cp) of "ORANGE" ghost
+   		in particular these are the starting position */
+		int cp_xOrange = 9;
+		int cp_yOrange = 9;
+		
+	/* 	They are the implementation of the previus seen function
+		and they contain the "AI" of the ghosts */
+	    void MoveGhosts(int* x, int* y, char ghost);
 //PacMan
-#include "PacManFunction.h"	
+//#include "PacManFunction.h"	
 
 int main()
 {
@@ -288,7 +437,7 @@ void displayMap()
 	}
 	
 	printf("\n \033[0;36m SCORE : %d ", score);
-	printf("\n\033[0;36mESC per uscire. Contatore: %i",counter);
+	printf("\n\033[0;36mESC per uscire ");
 
 	if(click == 27)
 		{
@@ -827,14 +976,15 @@ void move()
 			pinkEatable = true;
 			blueEatable = true;
 			orangeEatable = true;
-			timeToEat++;
-			if(timeToEat == 10)
+
+			counter = timeToEat;
+			if(timeToEat+10 == counter)
 			{
 				redEatable = false;
 				pinkEatable = false;
 				blueEatable = false;
 				orangeEatable = false;
-				timeToEat = 0;
+				
 				eatable=false;
 				ghostEated = 0;				
 			}
@@ -894,4 +1044,3 @@ void move()
 				}
 	}
 }
-
