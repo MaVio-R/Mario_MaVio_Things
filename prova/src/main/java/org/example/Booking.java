25d922973b06e9143f9916e2749798eaaeb8f8b7
Mario-jpeg <m.laurato04@gmail.com>
2025-04-22 15:20:37 +0200

    .

--- prova/src/main/java/org/example/Booking.java
+++ prova/src/main/java/org/example/Booking.java
@@ -3,7 +3,7 @@ package org.example;
 public class Booking {
     private String bookingCode;
     private String ticketNum;
-    private boolean seatAssigned;
+    private String seatAssigned;
     private BookingStatus status;
 
     public void checkIn(){
